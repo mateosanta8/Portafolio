d11b439637096d0c20bc5eb2b9a82e219ed406e5
Mateo Santa <mateosanta8@gmail.com>
2023-09-07 11:22:37 -0500

    proyecto terminado

--- add.js
+++ add.js
@@ -9,6 +9,15 @@ toggle_close.addEventListener('click', toggleMenu);
 
 function toggleMenu () {
   menu.classList.toggle('show-menu')
+
+  if(menu.classList.contains('show-menu')) {
+    toggle_open.style.display = 'none';
+    toggle_close.style.display = 'block';
+  }
+  else{
+    toggle_open.style.display = 'block';
+    toggle_close.style.display = 'none';
+  }
 }
 
 
